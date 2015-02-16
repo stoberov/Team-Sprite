3a815ee8a327b3f60cca68a194276167ae10cc87
Stoyan <stoyan.berov@gmail.com>
2015-02-16 22:23:55 +0200

    Added a simple intro animation

--- ConsoleGame/ConsoleGame/Program.cs
+++ ConsoleGame/ConsoleGame/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 
 class Program
 {
@@ -17,6 +19,42 @@ class Program
         //Hide cursor
         Console.CursorVisible = false;
 
-        Console.WriteLine("Test");
+        PrintTelerikAcademyLogo();
+    }
+
+    static void PrintTelerikAcademyLogo()
+    {
+        StreamReader logoTelerikAcademy = new StreamReader(@"..\..\telerik-logo.txt");
+
+        using (logoTelerikAcademy)
+        {
+            string line = logoTelerikAcademy.ReadLine();
+            int lineNum = 0;
+
+            while (line != null)
+            {
+                Console.SetCursorPosition(15, lineNum);
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                // Print the logo slowly
+                Thread.Sleep(100);
+                Console.WriteLine(line);
+
+                line = logoTelerikAcademy.ReadLine();
+                lineNum++;
+            }
+
+            Console.SetCursorPosition(30, lineNum);
+            Console.WriteLine("Telerik Academy");
+
+            Console.SetCursorPosition(22, lineNum + 1);
+            Console.WriteLine("A Console Game by Team Sprite");
+        }
+
+        // Pause the logo for 3secs
+        Thread.Sleep(3000);
+
+        // Clean the console to begin the game
+        Console.Clear();
     }
 }
