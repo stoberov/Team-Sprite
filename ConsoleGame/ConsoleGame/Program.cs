d592896ef2d55311c2e0515adc91f543a18eb83d
Stoyan <stoyan.berov@gmail.com>
2015-02-16 21:56:32 +0200

    First commit - set console borders, remove scroll, hide cursor

---
+++ ConsoleGame/ConsoleGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const int winWidth = 80;
+        const int winHeight = 40;
+
+        //Setting console borders
+        Console.SetWindowSize(winWidth, winHeight);
+
+        //Remove Scrolls
+        Console.BufferWidth = Console.WindowWidth;
+        Console.BufferHeight = Console.WindowHeight;
+
+        //Hide cursor
+        Console.CursorVisible = false;
+
+        Console.WriteLine("Test");
+    }
+}
